7bc3df619d60db19783c4c76f6a9bee447ad42e4
Nicholas Rehac <tup88289@temple.edu>
2024-02-28 16:29:05 -0500

    added scoped functions

--- app/src/main/java/edu/temple/scopefunctionactivity/MainActivity.kt
+++ app/src/main/java/edu/temple/scopefunctionactivity/MainActivity.kt
@@ -17,6 +17,12 @@ class MainActivity : AppCompatActivity() {
         // printing their output to the Log, which is visible in the LogCat:
         // eg. Log.d("function output", getTestDataArray().toString())
 
+        val testarray = getTestDataArray()
+        Log.d("1", testarray.toString())
+        Log.d("2", averageLessThanMedian(testarray.map { it.toDouble() }).toString())
+        Log.d("3", getView(0, null, testarray.toList(), this).toString())
+
+
     }
 
 
@@ -27,39 +33,25 @@ class MainActivity : AppCompatActivity() {
     // Look at the final/return value and build the function "working backwards"
 
     // Return a list of random, sorted integers
-    private fun getTestDataArray() : List<Int> {
-        val testArray = MutableList(10){ Random.nextInt()}
-        testArray.sort()
-        return testArray
-    }
+    private fun getTestDataArray() : List<Int>  = MutableList(10){ Random.nextInt()}.apply { sort() }
 
     // Return true if average value in list is greater than median value, false otherwise
-    private fun averageLessThanMedian(listOfNumbers: List<Double>): Boolean {
-        val avg = listOfNumbers.average()
-        val sortedList = listOfNumbers.sorted()
-        val median = if (sortedList.size % 2 == 0)
-            (sortedList[sortedList.size / 2] + sortedList[(sortedList.size - 1) / 2]) / 2
-        else
-            sortedList[sortedList.size / 2]
-
-        return avg < median
+    private fun averageLessThanMedian(listOfNumbers: List<Double>): Boolean = listOfNumbers.run{
+        average() < with(sorted()) {
+            if (this.size % 2 == 0)
+                (this[this.size / 2] + this[(this.size - 1) / 2]) / 2
+            else
+                this[this.size / 2]
+        }
     }
 
     // Create a view from an item in a collection, but recycle if possible (similar to an AdapterView's adapter)
-    private fun getView(position: Int, recycledView: View?, collection: List<Int>, context: Context): View {
-        val textView: TextView
-
-        if (recycledView != null) {
-            textView = recycledView as TextView
-        } else {
-            textView = TextView(context)
-            textView.setPadding(5, 10, 10, 0)
-            textView.textSize = 22f
+    private fun getView(position: Int, recycledView: View?, collection: List<Int>, context: Context): View = with(recycledView){
+        ((this as? TextView) ?: TextView(context)).apply {
+            setPadding(5, 10, 10, 0)
+            textSize = 22f
+            text = collection[position].toString()
         }
-
-        textView.text = collection[position].toString()
-
-        return textView
     }
 
 }
